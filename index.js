b1a471967ade8ba1d1b3a2c9b44e4ea8bbaf9659
zoe <53597160+zoeniu@users.noreply.github.com>
2021-11-13 12:20:24 +1300

    Render out user input

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    renderLeads();
 })
 
-let listItems = "";
-
+function renderLeads() {
+    let listItems = "";
 for (let i=0; i<myLeads.length; i++) {
     listItems += "<li>" + myLeads[i] + "</li>"
 }
-
 ulEl.innerHTML = listItems;
+}
