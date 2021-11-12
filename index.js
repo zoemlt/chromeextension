b41519450eca94353cb46a0e05f09afd439f3b8a
zoe <53597160+zoeniu@users.noreply.github.com>
2021-11-13 10:58:12 +1300

    JS array and input qs

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let myLeads = [];
+let inputEl = document.querySelector("#input-el");
+
 let inputBtn = document.querySelector("#input-btn");
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
