a1968366aa655d9f607ee32daf1672f694b08c25
zoe <53597160+zoeniu@users.noreply.github.com>
2021-11-13 11:28:26 +1300

    Create ul

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
