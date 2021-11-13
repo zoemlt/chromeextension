6c627903ff9876720f9b85cad704d079860e38e0
zoe <53597160+zoeniu@users.noreply.github.com>
2021-11-13 17:17:31 +1300

    Delete button to clear

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.querySelector("#delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
@@ -16,6 +16,12 @@ inputBtn.addEventListener("click", function() {
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads();
     console.log(myLeads);
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    ulEl.innerText = null;
 })
 
 function renderLeads() {
