5aea8ab96b7e9cb21e55ce5264d312f05748d498
zoe <53597160+zoeniu@users.noreply.github.com>
2021-11-13 16:58:57 +1300

    Saving in local storage for refresh

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 
-localStorage.setItem("myLeads", "www.example.com");
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads();
-    console.log(localStorage.getItem("myLeads"));
+    console.log(myLeads);
 })
 
 function renderLeads() {
