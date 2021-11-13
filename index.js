85f283fdf7e53d03756ced0024a787ad25f07fe0
zoe <53597160+zoeniu@users.noreply.github.com>
2021-11-13 17:28:16 +1300

    Refactoring code

--- index.js
+++ index.js
@@ -7,33 +7,32 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
+}
+
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+            `
+}
+ulEl.innerHTML = listItems;
 }
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads();
-    console.log(myLeads);
+    render(myLeads);
 })
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear();
     myLeads = [];
-    ulEl.innerText = null;
+    render(myLeads);
 })
-
-function renderLeads() {
-    let listItems = "";
-for (let i=0; i<myLeads.length; i++) {
-    listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-            </a>
-        </li>
-        `
-}
-ulEl.innerHTML = listItems;
-}
